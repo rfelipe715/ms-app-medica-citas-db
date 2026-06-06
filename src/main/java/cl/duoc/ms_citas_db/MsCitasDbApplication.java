2b17c3ca4dd6400547ddfd0eb1fb22c90e65c20a
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-06 09:55:01 -0400

    primer commit

---
+++ src/main/java/cl/duoc/ms_citas_db/MsCitasDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_citas_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCitasDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCitasDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_citas_db/MsCitasDbApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_citas_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCitasDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
